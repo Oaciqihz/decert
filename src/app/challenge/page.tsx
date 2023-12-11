990e70d96806b3980d5d8fb2b703b6fba04df7c0
Caoqizhi <2391580279@qq.com>
2023-12-11 17:26:48 +0800

    en: antd、app router

---
+++ src/app/challenge/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button, ConfigProvider } from 'antd';
+
+
+export default function Page() {
+
+
+    return (
+        <div>
+            <Button type="primary">CHALLENGE</Button>
+        </div>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button, ConfigProvider } from 'antd';
+
+
+export default function Home() {
+
+
+    return (
+        <ConfigProvider>
+          <div className="App">
+            <Button type="primary">INDEX</Button>
+          </div>
+        </ConfigProvider>
+    )
+}
