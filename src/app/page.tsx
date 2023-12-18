e41a6b6a9566e9aecc7d567ef3f172c6dcbf514b
Caoqizhi <2391580279@qq.com>
2023-12-18 11:12:44 +0800

    en: 头部fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,7 @@ export default function Home() {
 
     return (
       <ConfigProvider>
-        <div className="App">
-          <h1>Content</h1>
+        <div className="Home">
         </div>
       </ConfigProvider>
     )
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -15,31 +15,35 @@ export default function Header() {
 
     return (
         <AntdHeader id="Header">
-            {/* 头部左侧栏 */}
-            <div className="navbar-links">
-                {/* logo */}
-                <div className="logo">
-                    <Image 
-                        // fill 
-                        priority
-                        width={146}
-                        height={33.81}
-                        alt="logo" 
-                        src="/images/img/logo.png" 
-                    />
+            <div className="custom-bg-round"></div>
+            
+            <div className="content">
+                {/* 头部左侧栏 */}
+                <div className="navbar-links">
+                    {/* logo */}
+                    <div className="logo">
+                        <Image 
+                            // fill 
+                            priority
+                            width={146}
+                            height={33.81}
+                            alt="logo" 
+                            src="/images/img/logo.png" 
+                        />
+                    </div>
+                    {/* 链接 */}
+                    <Link href="/tutorials">教程</Link>
+                    <Link href="/challenges">挑战</Link>
+                    <Link href="/vitae">认证</Link>
                 </div>
-                {/* 链接 */}
-                <Link href="/tutorials">教程</Link>
-                <Link href="/challenges">挑战</Link>
-                <Link href="/vitae">认证</Link>
-            </div>
-            {/* 头部右侧栏 */}
-            <div className="navbar-btns">
-                <Button
-                    onClick={() => context?.showConnectModal()} 
-                    className="btn-connect"
-                    id='hover-btn-full'
-                >连接钱包</Button>
+                {/* 头部右侧栏 */}
+                <div className="navbar-btns">
+                    <Button
+                        onClick={() => context?.showConnectModal()} 
+                        className="btn-connect"
+                        id='hover-btn-full'
+                    >连接钱包</Button>
+                </div>
             </div>
         </AntdHeader>
     )
